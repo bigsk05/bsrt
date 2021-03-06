8a30e855e424962eb50ed7e000110bbf2a64ed3a
Bigsk <xiaxinzhe@xiaxinzhe.cn>
2021-03-07 00:06:37 +0800

    update v1.2

--- Contr/Arduino/BringSpark/BringSpark.ino
+++ Contr/Arduino/BringSpark/BringSpark.ino
@@ -1,68 +1,63 @@
 /*
-BringSpark Controller
-Author: Bigsk(https://xiaxinzhe.cn)
-Date: 2021.3.3 15:42
-Copyright GHINK Network Studio
+  BringSpark Controller
+  Author: Bigsk(https://xiaxinzhe.cn)
+  Date: 2021.3.6 23:19
+  Copyright GHINK Network Studio
 */
 
 #include <SPI.h>
 #include <Ethernet2.h>
 #include <Servo.h>
 
+//Info for the ethernet card
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(192, 168, 0, 3);
-EthernetClient client1,client2;
-IPAddress server(192, 168, 0, 2);//Server IP
-Servo myservo1,myservo2;
 
-unsigned long lastConnectionTime1,lastConnectionTime2 = 0;
-const unsigned long postingInterval1,postingInterval2 = 10L * 10L;
+//Define servers and servos
+EthernetServer server1(8001),server2(8002);
+Servo m1,m2;
 
 void setup() {
+  
+
+  //Open serial communications
   Serial.begin(9600);
+  //Start the Ethernet connection and the server
   Ethernet.begin(mac, ip);
-  myservo1.attach(6);
-  myservo2.attach(5);
+  server1.begin();
+  server2.begin();
+  Serial.print("Arduino is at:");
+  Serial.println(Ethernet.localIP());
 }
 
 void loop() {
-  if (client1.available()) {
-    int c = client1.read();
-    Serial.print("From 1:");
-    Serial.println(c);
-    myservo1.write(c);
+  // listen for controller
+  EthernetClient client1 = server1.available();
+  EthernetClient client2 = server2.available();
+  //Code for M1
+  if (client1) {
+    Serial.println("From M1:");
+    while (client1.connected()) {
+      if (client1.available()) {
+        char c = client1.read();
+        Serial.println(c);
+        m1.write(c);
+      }
+    }
+    client1.stop();
   }
-  if (client2.available()) {
-    int c = client2.read();
-    Serial.print("From 2:");
-    Serial.println(c);
-    myservo2.write(c);
-  }
-  if (millis() - lastConnectionTime1 > postingInterval1) {
-    httpRequest1();
-  }
-  if (millis() - lastConnectionTime2 > postingInterval2) {
-    httpRequest2();
-  }
-}
-
-void httpRequest1() {
-  client1.stop();
-
-  if (client1.connect(server, 8001)) {//Server Port
-    client1.println();
-
-    lastConnectionTime1 = millis();
-  }
-}
-void httpRequest2() {
-  client2.stop();
-
-  if (client2.connect(server, 8002)) {//Server Port
-    client2.println();
-
-    lastConnectionTime2 = millis();
+  //Code for M2
+  if (client2) {
+    Serial.println("From M2:");
+    while (client2.connected()) {
+      if (client2.available()) {
+        char c = client2.read();
+        Serial.println(c);
+        m2.write(c);
+      }
+    }
+    client2.stop();
   }
 }
